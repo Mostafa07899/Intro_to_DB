fe5f5396eb3307449acc3c156e585e4cba8414ac
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-10-20 23:46:50 +0300

    add book store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,7 +21,7 @@ CREATE TABLE Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address text
+    address TEXT
 );
 
 CREATE TABLE Orders (
