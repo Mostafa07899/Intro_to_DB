758847b9cd3817e8868ef3ade732c49e952b07a6
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-10-21 00:00:36 +0300

    add MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '01002450854@Sasa'
+        )
+    
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created sucessfully!")
+
+
+    except error as e:
+        print(f"Error connecting to MySQL: {e}")
+
+
+    finally:
+        if connection.is_secured():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed")
+
+if __name__ == "__main__":
+    create_database()
+             
+
+
+    
